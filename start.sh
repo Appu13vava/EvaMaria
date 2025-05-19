cc4a33bae5f19ac5c20250ab086c62fa9b7390af
Appu13vava <94307842+Appu13vava@users.noreply.github.com>
2025-05-19 15:23:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,22 @@
-if [ -z $UPSTREAM_REPO ]
-then
+#!/bin/bash
+
+# Clone the correct repo
+if [ -z "$UPSTREAM_REPO" ]; then
   echo "Cloning main Repository"
   git clone https://github.com/AM-ROBOTS/EvaMaria.git /EvaMaria
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone "$UPSTREAM_REPO" /EvaMaria
 fi
-cd /EvaMaria
+
+cd /EvaMaria || exit
+
+# Install any updated requirements
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+
+# Start dummy Flask app for Koyeb health check
+python3 healthcheck.py &
+
+# Start the actual bot
+echo "Starting Bot..."
 python3 bot.py
